10702dd516a2dfb8d06b5bdd2b314ade0335fdaa
Nokuthaba <nokuthaba.m@outlook.com>
2024-09-26 16:25:26 +0200

    More on conditionals

--- Conditionals/src/Hello.java
+++ Conditionals/src/Hello.java
@@ -6,7 +6,22 @@ public class Hello {
             System.out.print("It is not an alien!");
         }
         else {
-            System.out.print("I am terrified of Aliens!");
+            System.out.println("I am terrified of Aliens!");
+        }
+
+        int topScore = 80;
+        if (topScore <= 100) {
+            System.out.println("You got the high score");
+        }
+        // Logical AND operator. Both sides must be true "&&"
+        int secondTopScore = 60;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        // Logical OR operator either expression can be true. "||"
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
         }
 
     }
